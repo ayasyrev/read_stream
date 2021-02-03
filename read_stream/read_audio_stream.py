5455c656eac326f8a816ce7a27e71e03366a1770
ayasyrev <a.yasyrev@gmail.com>
2021-02-03 17:10:06 +0000

    check data dir exists

--- read_stream/read_audio_stream.py
+++ read_stream/read_audio_stream.py
@@ -1,10 +1,15 @@
 from read_stream.stream import AudioStream
+from pathlib import Path
 
 
 def read_audio_stream(url, stream_name):
     stream = AudioStream(url)
     if stream.audio_stream:
         print('audio stream availible')
-        stream.read_audio_stream('data/' + stream_name)
+        dir_to_save = Path('data')
+        if dir_to_save.exists():
+            stream.read_audio_stream('data/' + stream_name)
+        else:
+            print("Error: no DATA dir.")
     else:
         print('NO audio stream.')
