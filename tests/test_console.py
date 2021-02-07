2d0bfdfb1326f197fad6ae6b74b8fc659a808341
ayasyrev <a.yasyrev@gmail.com>
2021-02-07 12:12:31 +0000

    tests, conf for tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,13 +1,48 @@
+import pytest
 from typer.testing import CliRunner
 
 from read_stream.console import app
 
 from read_stream import __version__
 
+mock_result = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+
+
+@pytest.fixture
+def mock_streamlink_streams(mocker):
+    mock = mocker.patch("streamlink.streams")
+    mock.return_value = mock_result
+    return mock
+
+
+def test_mock(mock_streamlink_streams):
+    import streamlink
+    stream_mocked = streamlink.streams('')
+    print('Test stream mock')
+    print(stream_mocked)
+    assert stream_mocked.keys() == mock_result.keys()
+    # assert stream == mock_result
+    print(stream_mocked.keys())
+
+
 runner = CliRunner()
 
 
-def test_app():
+def test_main_invokes_streamlink_streams(mock_streamlink_streams):
+    runner.invoke(app, ['fake_url', 'name'], input='\n')
+    assert mock_streamlink_streams.called
+
+
+def test_app_use_url(mock_streamlink_streams):
+    runner.invoke(app, ['fake_url', 'name'])
+    args, _ = mock_streamlink_streams.call_args
+    assert 'fake_url' in args
+
+
+def test_app(mock_streamlink_streams):
     url = 'aaa'
     url_2 = 'bbb'
     # default_save_name = 'stream'
